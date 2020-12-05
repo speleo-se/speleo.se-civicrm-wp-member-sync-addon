0a8937a6d714b47d68593eaf886cbfc23ac32a70
lasse <lasse@dev-lasse>
2020-12-05 10:24:57 +0100

    Mer utveckling

--- CiviCRMSpeleoSeConfig.php
+++ CiviCRMSpeleoSeConfig.php
@@ -2,55 +2,29 @@
 
 /**
  * Plugin Name: Speleo.se CiviCRM Conifg
- * Description: Konfigurerar CiviCRM för Sveriges Speleologförbund. Medlemskap.
+ * Description: Konfigurerar CiviCRM för Sveriges Speleologförbund med medlemskap. Körs när pluginen aktiveras. Kan sedan inaktiveras.
  *
  *
  * Läs: https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (! defined ( 'WPINC' )) {
+	die ();
 }
-
 class CiviCRMSpeleoSeConfig {
-	private $sektioner = [];
-
+	protected $sektioner = [ ];
+	protected $org_ssf;
 	public function configureCiviCRM() {
 		// Set default organisation:
-		$org_ssf = \Civi\Api4\Contact::update()
-				->addWhere('id', '=', 1)
-				->addValue('organization_name', 'Sveriges Speleologförbund')
-				->addValue('legal_name',        'Sveriges Speleologförbund SSF' )
-				->addValue('nick_name',         'SSF' )
-				->setLimit(1)
-				->setCheckPermissions(FALSE)
-				->execute()
-				->first();
-
-		foreach ( [ 
-				'Dyksektionen',
-				'Gruvsektionen',
-				'Vertikala sektionen'
-				] as $sektion ) {
-			$this->sektioner [$sektion] = \Civi\Api4\Contact::get()
-					->addWhere('contact_type', '=', 'Organization')
-					->addWhere ( 'organization_name', 'LIKE', '%' . $sektion . '%' )
-					->setLimit(1)
-					->setCheckPermissions(FALSE)
-					->execute()
-					->first();
-			if ($this->sektioner [$sektion] === null) {
-				$this->sektioner [$sektion] = \Civi\Api4\Contact::create()
-						->addValue('organization_name', 'SSF - '.$sektion)
-						->addValue('legal_name',        'Sveriges Speleologförbund - '.$sektion)
-						->addValue('nick_name',         $sektion)
-						->setLimit(1)
-						->setCheckPermissions(FALSE)
-						->execute()
-						->first();
-			}
-		}
+		$this->org_ssf = \Civi\Api4\Contact::update ( false )
+				->addWhere ( 'id', '=', 1 )
+				->addValue ( 'organization_name', 'Sveriges Speleologförbund' )
+				->addValue ( 'legal_name', 'Sveriges Speleologförbund SSF' )
+				->addValue ( 'nick_name', 'SSF' )
+				->execute ()
+				->first ();
+		
 		$calenderYearMemebership = [ 
 				'duration_unit' => "year",
 				'duration_interval' => 1,
@@ -58,13 +32,31 @@ class CiviCRMSpeleoSeConfig {
 				"fixed_period_start_day" => "0101",
 				'fixed_period_rollover_day' => "1201" // När blir registrerat medlemskap för nästa år?
 		];
-		
-		//SSF-standard-medlemskap:
+
+		// SSF familjemedlemskapsrealtion
+		$familjemedlemskapsrealtion = \Civi\Api4\RelationshipType::get ( false )
+				->addWhere ( 'name_a_b', '=', 'huvudfamiljemedlem' )
+				->setLimit ( 1 )
+				->execute ()
+				->first ()
+				?? \Civi\Api4\RelationshipType::create ( true )
+					->addValue ( 'name_a_b', 'huvudfamiljemedlem' )
+					->addValue ( 'label_a_', 'familjemedlemskap betalas av' )
+					->addValue ( 'name_b_a', 'familjemedlem' )
+					->addValue ( 'label_b_a', 'betalar familjemedlemskap till' )
+					->addValue ( 'description', 'SSF familjemedlemskapsrelation' )
+					->addValue ( 'contact_type_a', 'Individual' )
+					->addValue ( 'contact_type_b', 'Individual' )
+					->addValue ( 'is_active', 1 )
+					->execute ()
+					->first ();
+
+		// SSF-standard-medlemskap:
 		$this->createMembershipType ( [ 
 				'name' => "Vuxen",
-				'description' => "Medlemskap under kalenderåret för person över 26 år.",
+				'description' => "Medlemskap under kalenderåret för person från 26 år.",
 				'minimum_fee' => "300"
-				] + $calenderYearMemebership );
+		] + $calenderYearMemebership );
 		$this->createMembershipType ( [ 
 				'name' => "Student",
 				'description' => "Medlemskap under kalenderåret för person under 26 år.",
@@ -73,72 +65,102 @@ class CiviCRMSpeleoSeConfig {
 		$this->createMembershipType ( [ 
 				'name' => "Familj",
 				'description' => "Medlemskap under kalenderåret för person i samma hushåll.",
-				'minimum_fee' => "350"
+				'minimum_fee' => "350",
+
+				'relationship_type_id' => $familjemedlemskapsrealtion ['id'],
+				'relationship_direction' => 'b_a'
 		] + $calenderYearMemebership );
-		
+
 		// Hedersmedlem
 		$this->createMembershipType ( [ 
 				'name' => "Hedersmedlem",
 				'description' => "Hedersmedlemskap livet ut.",
 				'minimum_fee' => "0",
 				"visibility" => "Admin",
-	
+
 				'duration_unit' => "lifetime",
 				'duration_interval' => 1,
 				'period_type' => "rolling"
 		] );
 
 		// Sektioner
-		foreach ( [
+		foreach ( [ 
 				'Dyksektionen' => 45,
-				'Gruvsektionen' => 15,
+				'Gruvsektionen' => 15.,
 				'Vertikala sektionen' => 15.
-				] as $sektion => $medlemsavgift ) {
-			$this->sektioner[$sektion] = \Civi\Api4\Contact::get()
-			->addWhere('contact_type', '=', 'Organization')
-			->addWhere ( 'organization_name', 'LIKE', '%' . $sektion . '%' )
-			->setLimit(1)
-			->setCheckPermissions(FALSE)
-			->execute()
-			->first();
-			if ($this->sektioner [$sektion] === null) {
-				$this->sektioner [$sektion] = \Civi\Api4\Contact::create()
-				->addValue('organization_name', 'SSF - '.$sektion)
-				->addValue('legal_name',        'Sveriges Speleologförbund - '.$sektion)
-				->addValue('nick_name',         $sektion)
-				->setLimit(1)
-				->setCheckPermissions(FALSE)
-				->execute()
-				->first();
-			}
-			$this->createMembershipType ( [
+		] as $sektion => $medlemsavgift ) {
+			$this->sektioner [$sektion] = \Civi\Api4\Contact::get ( FALSE )
+					->addWhere ( 'contact_type', '=', 'Organization' )
+					->addWhere ( 'organization_name', 'LIKE', '%' . $sektion . '%' )
+					->setLimit ( 1 )
+					->execute ()
+					->first ()
+					?? \Civi\Api4\Contact::create ( false )
+						->addValue ( 'organization_name', 'SSF - ' . $sektion )
+						->addValue ( 'legal_name', 'Sveriges Speleologförbund - ' . $sektion )
+						->addValue ( 'nick_name', $sektion )
+						->execute ()
+						->first ();
+			$this->createMembershipType ( [ 
 					'name' => $sektion,
-					'description' => "Medlem i ".$sektion.".",
+					'description' => "Medlem i " . $sektion . ".",
 					'minimum_fee' => $medlemsavgift,
-					'member_of_contact_id' => $this->sektioner[$sektion]['id'],
-					] + $calenderYearMemebership );
-			
+					'member_of_contact_id' => $this->sektioner [$sektion] ['id']
+			] + $calenderYearMemebership );
+		}
+		// ########################################
+		// ### Dölj data som SSF inte använder: ###
+		// ########################################
+
+		// Dölj relationer som inte börjar på SFF i description:
+		$relationshipTypes = \Civi\Api4\RelationshipType::update ( false )
+				->addWhere ( 'description', 'NOT LIKE', 'SSF%' )
+				->addValue ( 'is_active', FALSE )
+				->execute ();
+		
+		// Allmäna inställningar:
+		foreach ( [
+						'defaultCurrency' => 'SEK',
+				] as $key => $value ) {
+			\Civi\Api4\Setting::set(false)
+					->addValue($key, $value)
+					->execute();
 		}
 	}
-	private function createMembershipType(Array $membershipTypeOptions) {
-		$result = civicrm_api3('MembershipType', 'get', [
-				'sequential' => 1,
-				'name' => $membershipTypeOptions['name'],
-				]);
-		if (!$result['values']) {
-			$mergedMembershipTypeOptions = 
-					$membershipTypeOptions
-					+ [
-							'member_of_contact_id' => 1, //SSF
-							'is_active' => 1,
-							"visibility" => "Public",
-							'minimum_fee' => "0",
-							"financial_type_id" => "2", // ("Member Dues))
-					];
-			$result = civicrm_api3('MembershipType', 'create', $mergedMembershipTypeOptions);
+	protected function createMembershipType(Array $membershipTypeOptions) {
+		$result = \Civi\Api4\MembershipType::get ( FALSE )
+				->addWhere ( 'name', '=', $membershipTypeOptions ['name'] )
+				->setLimit ( 1 )
+				->execute ()
+				->first ();
+
+		// $result = civicrm_api3('MembershipType', 'get', [
+		// 'sequential' => 1,
+		// 'name' => $membershipTypeOptions['name'],
+		// ]);
+		// if (!$result['values']) {
+		if (! $result) {
+			$mergedMembershipTypeOptions = $membershipTypeOptions + [ 
+					'member_of_contact_id' => 1, // SSF
+					'is_active' => 1,
+					"visibility" => "Public",
+					'minimum_fee' => 0,
+					"financial_type_id" => 2 // ("Member Dues))
+			];
+
+			// $result = civicrm_api3('MembershipType', 'create', $mergedMembershipTypeOptions);
+			$query = \Civi\Api4\MembershipType::create ( false );
+			foreach ( $mergedMembershipTypeOptions as $fieldName => $mergedMembershipTypeOption ) {
+				$query->addValue ( $fieldName, $mergedMembershipTypeOption );
+			}
+			$query->execute ()
+					->first ();
 		}
 	}
 }
 
 // plugin activation
-register_activation_hook( __FILE__, [ new CiviCRMSpeleoSeConfig(), 'configureCiviCRM' ] );
+register_activation_hook ( __FILE__, [ 
+		new CiviCRMSpeleoSeConfig (),
+		'configureCiviCRM'
+] );
--- civicrm-wp-member-sync-speleo-se-addon.php
+++ civicrm-wp-member-sync-speleo-se-addon.php
@@ -1,10 +1,18 @@
 <?php
-use Civi\Api4\OptionValue;
 
 /**
  * Plugin Name: Speleo.se CiviCRM addon
+ * Description: Sveriges Speleologförbund med medlemskap.
+ * 
+ * Inspired by: https://github.com/christianwach/civicrm-wp-member-sync/issues/16#issuecomment-276852524
+ * 
  */
 
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
 class CiviCRMSpeleoSe {
 	public function __construct() {
@@ -21,40 +29,60 @@ class CiviCRMSpeleoSe {
 		// The handler will execute with 'priority' 10 and accept 2 arguments.
 
 	    /**
-	     * Handler for the filter 'civi_wp_member_sync_new_username'.
-	     * Uses the $civi_contact argument to create a username with the first letter of the first name and the last name.
-	     * @see Civi_WP_Member_Sync_Users
+	     * skapa medlemsnummer och använd detta till användarnamn:
 	     */
-	    add_filter('civi_wp_member_sync_new_username', [$this, 'civi_wp_member_sync_new_username'], 10 , 2);
+		add_filter('civi_wp_member_sync_new_username', [$this, 'civi_wp_member_sync_new_username'], 10 , 2);
 
-	    
-	    
 		/**
-		 * Let other plugins know that we're about to sync all users.
-		 * @see Civi_WP_Member_Sync_Admin
+		 * Sätta flagga att vi gör en syn all!
 		 */
 	    add_action('civi_wp_member_sync_pre_sync_all', [$this, 'civi_wp_member_sync_pre_sync_all'], 10 , 0);
 		/**
-		 * Let other plugins know that we've synced all users.
-         * @see Civi_WP_Member_Sync_Admin 
+		 * Ta bort flagga sync all.
 		 */
 	    add_action('civi_wp_member_sync_after_sync_all', [$this, 'civi_wp_member_sync_after_sync_all'], 10 , 0);
 
 		
-		
-		/**
-		 * Let other plugins know that we've inserted a user.
-		 * @see Civi_WP_Member_Sync_Users
-		 */
-	    add_action('civi_wp_member_sync_after_insert_user', [$this, 'civi_wp_member_sync_after_insert_user'], 10, 2);
-		
+	    /**
+	     * Broadcast that we've inserted a user.
+	     *
+	     * This action fires before the hooks are re-added which can be useful
+	     * if callbacks perform actions that in some way update the WordPress
+	     * User, for example sending emails via `wp_new_user_notification()`.
+	     *
+	     * @see wp_new_user_notification()
+	     *
+	     * @param array $civi_contact The CiviCRM contact object.
+	     * @param int $user_id The numeric ID of the WordPress user.
+	     */
+	    add_action('civi_wp_member_sync_post_insert_user', [$this, 'civi_wp_member_sync_post_insert_user'], 10, 4);
+
+	    
 		/**
 		 * Broadcast the membership update.
 		 * @see Civi_WP_Member_Sync_Members
 		 */
 	    add_action('civi_wp_member_sync_membership_updated', [$this, 'civi_wp_member_sync_membership_updated'], 10, 4);
-		
+	    
+	    /**
+	     * Filters the contents of the new user notification email sent to the new user.
+	     *
+	     * @since 4.9.0
+	     *
+	     * @param array   $wp_new_user_notification_email {
+	     *     Used to build wp_mail().
+	     *
+	     *     @type string $to      The intended recipient - New user email address.
+	     *     @type string $subject The subject of the email.
+	     *     @type string $message The body of the email.
+	     *     @type string $headers The headers of the email.
+	     * }
+	     * @param WP_User $user     User object for new user.
+	     * @param string  $blogname The site title.
+	     */
+	    add_filter('wp_new_user_notification_email', [$this, 'wp_new_user_notification_email'], 10 , 2);
 	}
+	
 	/**
 	 * Handler for the filter 'civi_wp_member_sync_new_username'.
 	 * Uses the $civi_contact argument to create a username with the first letter of the first name and the last name.
@@ -64,14 +92,48 @@ class CiviCRMSpeleoSe {
 	 * @return string $user_name The modified WordPress username
 	 */
 	public function civi_wp_member_sync_new_username($user_name, $civi_contact) {
-		$contact_user_name = $civi_contact['nick_name'];
+		// Username för WordPress är medlemsnumret.
+		// Om användaren inte fått något medlemsnummer så skapar vi det, genom att välja det högsta och lägga till ett.
+		if (!$civi_contact['external_identifier']) {
+			// Skapa ett nytt medlemsnummer!
+			
+			// Hämta högst som finns hittills:
+			$contact = \Civi\Api4\Contact::get()
+			->setSelect([
+					'external_identifier',
+			])
+			// Om det av någon anledning blir så att det finns någon som har satts till medlemsnummer 9999999 eller liknande.
+			// Så går det att lösa det med att lägga in filer här:
+			// ->addWhere('external_identifier', '<', 9999999)
+			->addOrderBy('external_identifier', 'DESC')
+			->setLimit(1)
+			->setCheckPermissions(FALSE)
+			->execute()
+			->first();
+			
+			// Nytt medlemsnummer, ett mer än det högsta som finns innan.
+			$next_external_identifier = strval($contact['external_identifier'] + 1);
+			
+			$civi_contact = \Civi\Api4\Contact::update()
+			->addWhere('id', '=', $civi_contact['id'])
+			->addValue('external_identifier', $next_external_identifier)
+			->setLimit(1)
+			->setCheckPermissions(FALSE)
+			->execute()
+			->first();
+			
+			$this->addActivity(
+					$civi_contact['id']
+					, sprintf("Nytt medlemsnummer: %s", $next_external_identifier)
+					, self::ACTIVITYTYPE_NEWMEMBERSHIPNR
+					);
+			
+		}
+		$contact_user_name = $civi_contact['external_identifier'];
 		
 		// $contact_user_name = sanitize_title( sanitize_user( $contact_user_name ) );
 		return $contact_user_name;
 	}
-
-	
-	
 	
 	private $ongoing_sync_all = false;
 	public function civi_wp_member_sync_pre_sync_all() {
@@ -81,99 +143,170 @@ class CiviCRMSpeleoSe {
 	    $this->ongoing_sync_all = false;
 	}
 	
+	/**
+	 * @see Civi_WP_Member_Sync_Members
+	 * Broadcast that we've inserted a user.
+	 * @see wp_new_user_notification()
+	 *
+	 * This action fires before the hooks are re-added which can be useful
+	 * if callbacks perform actions that in some way update the WordPress
+	 * User, for example sending emails via `wp_new_user_notification()`.
+	 *
+	 * @param array $civi_contact The CiviCRM contact object.
+	 * @param int $user_id The numeric ID of the WordPress user.
+	 *
+	 */
+	public function civi_wp_member_sync_post_insert_user($civi_contact, $user_id) {
+		// Skicka ut ett välkomstmail till användaren:
+		// TODO ändra till user eller both:
+		// Aktivera vid release.
+		$notify = 'admin';
+		wp_new_user_notification($user_id, null, $notify);
+		
+		$wpUser = get_user_by('id', $user_id);
+		$this->addActivity(
+				$civi_contact['id']
+				, sprintf("Mail skickat till '%s' för användare '%s'.", $wpUser->user_email, $wpUser->user_login)
+				, self::ACTIVITYTYPE_WORDPRESS
+				, 'Notify: '.$notify.'<br>En Wordpress-användare har skapats. <br><pre>'.htmlentities(print_r($wpUser, true)).'</pre>'
+				);
+		
+	}
 	
 	/**
-	 * Member Sync: Let other plugins know that we've inserted a user.
-	 * Create an activity on the user. 
-	 * @param array $civi_contact
-	 * @param int $user_id
+	 * Member Sync: Broadcast the membership update.
+	 * 
+	 * @param string $op The type of operation.
+	 * @param WP_User $user The WordPress user object.
+	 * @param object $objectRef The CiviCRM membership being updated.
+	 * @param object $previous_membership The previous CiviCRM membership if this is a renewal.
 	 */
-	public function civi_wp_member_sync_after_insert_user($civi_contact, $user_id ) {
-	    $wpUser = get_user_by('id', $user_id);
-	    // Oklart om man kan göra såhär. Massa logik i WP_User...
-	    $wpUser->data->user_pass = '*****';
-	    \Civi\Api4\Activity::create()
-        	    ->addValue('source_contact_id', $civi_contact['id'])
-        	    ->addValue('activity_type_id', $this->getActivityTypeIdSyncUser())
-        	    ->addValue('subject', "Wordpress-användare '".$wpUser->user_login."' skapad")
-        	    ->addValue('details', ($this->ongoing_sync_all ? '<strong>Sync all!</strong>' : '').'<pr>En Wordpress-användare har skapats. <br><pre>'.htmlentities(print_r($wpUser, true)).'</pre>')
-        	    ->addValue('activity_date_time', date("Y-m-d H:i:s"))
-        	    ->addValue('status_id', 2) // Slutförd
-        	    ->addValue('priority_id', 2) // Normal
-        	    ->setCheckPermissions(FALSE)
-        	    ->execute();
-    }
-
-    
-    /**
-     * Member Sync: Broadcast the membership update.
-     * @param string $op The type of operation.
-     * @param WP_User $user The WordPress user object.
-     * @param object $objectRef The CiviCRM membership being updated.
-     * @param object $previous_membership The previous CiviCRM membership if this is a renewal.
+	public function civi_wp_member_sync_membership_updated($op, $wpUser, $membership, $previous_membership ) {
+		$this->addActivity(
+				$membership->contact_id
+				, sprintf("Wordpress-användare '%s' uppdaterad.", $wpUser->user_login)
+				, self::ACTIVITYTYPE_SYNCUSER
+				, 'Operation: '.$op.'<br>'
+						.'<br>En Wordpress-användare har ändrat medelemsstatus. <br>'
+						.'<pre>'.htmlentities(print_r($wpUser, true)).'</pre>'
+						.'Medlemskap: '
+						.'<pre>'.htmlentities(print_r($membership, true)).'</pre>'
+				);
+	}
+	
+	/**
+     * Filters the contents of the new user notification email sent to the new user.
+     *
+     * @since 4.9.0
+     *
+     * @param array   $wp_new_user_notification_email {
+     *     Used to build wp_mail().
+     *
+     *     @type string $to      The intended recipient - New user email address.
+     *     @type string $subject The subject of the email.
+     *     @type string $message The body of the email.
+     *     @type string $headers The headers of the email.
+     * }
+     * @param WP_User $user     User object for new user.
+     * @param string  $blogname The site title.
      */
-    public function civi_wp_member_sync_membership_updated($op, $wpUser, $membership, $previous_membership ) {
-        // Oklart om man kan göra såhär. Massa logik i WP_User...
-        $wpUser->data->user_pass = '*****';
-        \Civi\Api4\Activity::create()
-        ->addValue('source_contact_id', $membership->contact_id)
-        ->addValue('activity_type_id', $this->getActivityTypeIdSyncUser())
-        ->addValue('subject', "Wordpress-användare '".$wpUser->user_login."' uppdaterad")
-        ->addValue('details',
-            ($this->ongoing_sync_all ? '<strong>Sync all!</strong>' : '')
-            .'Operation: '.$op.'<br>'
-            .'<pr>En Wordpress-användare har ändrat medelemsstatus. <br>'
-            .'<pre>'.htmlentities(print_r($wpUser, true)).'</pre>'
-            .'Medlemskap: '
-            .'<pre>'.htmlentities(print_r($membership, true)).'</pre>'
-            )
-        ->addValue('activity_date_time', date("Y-m-d H:i:s"))
-        ->addValue('status_id', 2) // Slutförd
-        ->addValue('priority_id', 0) // Normal
-        ->setCheckPermissions(FALSE)
-        ->execute();
-    }
-
-    private function getActivityTypeIdSyncUser() {
-        return $this->getActivityTypeId(
-            'Syncning av användare'
-            , 'Syncningshändelser mellan CiviCRM-kontakter och Wordpress-användare för inloggning på hemsidan.'
-            , 'fa-refresh'
-            );
-    }
-    
-    private $cachedActivityTypeId = [];
-    /**
-     * 
+	public function wp_new_user_notification_email( $wp_new_user_notification_email, $user, $blogname )  {
+		// Ska vi lägga till mer info i välkomstmailen?
+		
+		return $wp_new_user_notification_email;
+	}
+	
+	
+	private const ACTIVITYTYPE_WORDPRESS = 'ActivityTypeWordpress';
+	private const ACTIVITYTYPE_SYNCUSER = 'ActivityTypeSyncUser';
+	private const ACTIVITYTYPE_NEWMEMBERSHIPNR = 'ActivityTypeNewMembershipNr';
+	protected function addActivity($contactId, $subject, $type, $details = '') {
+			\Civi\Api4\Activity::create(false)
+			->addValue('source_contact_id', $contactId)
+			->addValue('activity_type_id', $this->getActivityTypeId($type))
+			->addValue('subject', $subject)
+			->addValue('details',
+					($this->ongoing_sync_all ? '<strong>Sync all batch.</strong>' : '')
+					. $details)
+			->addValue('activity_date_time', date("Y-m-d H:i:s"))
+			->addValue('status_id', 2) // Slutförd
+			->addValue('priority_id', 0) // Normal
+			->setCheckPermissions(FALSE)
+			->execute();
+	}
+	protected function getActivityTypeId($type) {
+		switch ($type) {
+			case self::ACTIVITYTYPE_WORDPRESS:
+				return $this->getActivityTypeId(
+						'Välkomstmail från Wordpress'
+						, 'Ett välkomstmail för inloggning till Wordpress skickas ut när användare skapas.'
+						, 'fa-wordpress'
+						);
+			case self::ACTIVITYTYPE_SYNCUSER:
+				return $this->getActivityTypeId(
+						'Syncning av användare'
+						, 'Syncningshändelser mellan CiviCRM-kontakter och Wordpress-användare för inloggning på hemsidan.'
+						, 'fa-refresh'
+						);
+			case self::ACTIVITYTYPE_NEWMEMBERSHIPNR:
+				return $this->getActivityTypeId(
+						'Skapat medlemsnummer'
+						, 'Vid synkning av Wordpress-användare skapas nytt medlemsnummer.'
+						, 'fa-user-plus'
+						);
+		}
+	}
+	protected $cachedActivityTypeId = [];
+	/**
+     * Returns ActvityType with matching label. If none existing, creates a new!
+     * Cachad. 
      * @param string $label
      * @param string $description
      * @return int
      */
-    private function getActivityTypeId($label, $description = null, $icon = null) {
+    protected function getActivityTypeId($label, $description = null, $icon = null) {
         if (!isset($this->cachedActivityTypeId[$label])) {
-            $optionValue =
+            $this->cachedActivityTypeId[$label] =
                     \Civi\Api4\OptionValue::get()
                             ->setSelect(['value'])
+                            ->addWhere('option_group.name', '=', 'activity_type')
                             ->addWhere('label', '=', $label)
                             ->setLimit(1)
                             ->setCheckPermissions(FALSE)
                             ->execute()
-                            ->first()
-                    ?? \Civi\Api4\OptionValue::create()
-                            ->addValue('option_group_id', 2)
-                            ->addValue('label', $label)
-                            ->addValue('is_active', 1)
-                            ->addValue('description', $description)
-                            ->addValue('icon', $icon)
+                            ->first()['value']
+                    // ::get()...->first() ger null om den inte finns, skapa och returnera då en ny aktivitetstyp: 
+                    ?? \Civi\Api4\OptionGroup::get()
+                            ->setSelect(['id'])
+                            ->addWhere('name', '=', 'activity_type')
+                            ->setLimit(1)
+                            ->setChain([
+                                'create_option_value' => [
+                                    'OptionValue',
+                                    'create',
+                                    [
+                                        'values' => [
+                                            'option_group_id' => '$id', //id from OptionGroup::get()
+                                            'label'           => $label,
+                                            'is_active'       => 1,
+                                            'description'     => $description,
+                                            'icon'            => $icon
+                                        ]
+                                    ],
+                                    0 // Samma som: ->first()
+                                ],
+                            ])
                             ->setCheckPermissions(FALSE)
                             ->execute()
-                            ->first();
-            $this->cachedActivityTypeId[$label] = $optionValue['value'];
+                            ->first()['create_option_value']['value'];
         }
-        
         return $this->cachedActivityTypeId[$label];
     }
 } // class ends
 
 // init plugin
 new CiviCRMSpeleoSe();
+
+// plugin activation
+//register_activation_hook( __FILE__, array( $flying_gators, 'activate' ) );
+
